2caf8bdcb41b1ef3bae2eec77be85b389ca87293
cozenakai <masaki2970@icloud.com>
2020-04-21 02:07:37 +0900

    add hr

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,16 @@
   <div class="container">
     <div>
     <Counter></Counter>
+    <hr>
     <Counter></Counter>
+    <hr>
     <Counter></Counter>
+    <hr>
     </div>
   </div>
 </template>
 
 <script>
-
 
 import Counter from '~/components/Counter.vue'
 export default {
