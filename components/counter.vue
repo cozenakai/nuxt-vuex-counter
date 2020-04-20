9604bcd4a1f0fe8f724da810630dd3bf3bbd3c5d
cozenakai <masaki2970@icloud.com>
2020-04-21 02:05:44 +0900

    add title

--- components/counter.vue
+++ components/counter.vue
@@ -1,5 +1,6 @@
 <template>
     <section>
+        <h2>{{ title }}</h2>
         counter: {{ $store.state.counter.counter}}
         <button v-on:click="$store.commit('counter/countUp')">+1</button>
         <button v-on:click="$store.commit('counter/reset')">reset</button>
