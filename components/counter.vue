1f5f5c029d3f17839e0262d3c529ab1838bf9c80
cozenakai <masaki2970@icloud.com>
2020-04-21 01:52:09 +0900

    second commit

---
+++ components/counter.vue
@@ -0,0 +1,16 @@
+<template>
+    <section>
+        counter: {{ $store.state.counter.counter}}
+        <button v-on:click="$store.commit('counter/countUp')">+1</button>
+    </section>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {title: 'Counter App'
+
+        }
+    }
+}
+</script>
